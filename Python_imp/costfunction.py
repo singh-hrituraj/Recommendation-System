ba34589489bb517e599d42c7375961d5df6d1916
Hrituraj Singh <hrituraj1997@gmail.com>
2017-03-10 01:32:01 +0530

    created costfunction
    Calculates the cost of the current function parameters' output

---
+++ Python_imp/costfunction.py
@@ -0,0 +1,29 @@
+import load_data
+import numpy as np
+
+X = load_data.X
+Y = load_data.Y
+R = load_data.R
+Theta = load_data.Theta
+numusers = load_data.numusers
+nummovies = load_data.nummovies
+numfeatures = load_data.numfeatures
+
+
+def costfunction(X, Theta, Y, R, numusers, nummovies,numfeatures, reg = 0):
+
+    X = X[0:nummovies,0:numfeatures]
+    print X
+    Y = Y[0:nummovies,0:numusers]
+    Theta = Theta[0:numusers, 0:numfeatures]
+    R = R[0:nummovies, 0:numusers]
+    print R
+
+    Output = np.dot(X, np.transpose(Theta))
+
+    J = 0.5 * np.sum((Output[R]- Y[R])*(Output[R] - Y[R]))
+    return J
+
+costfunction(X,Theta, Y, R, numusers, nummovies, numfeatures)
+
+
