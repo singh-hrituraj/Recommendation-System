2792ab4474bd8ef0912d2fb17942aa6fa947c4a2
Hrituraj Singh <hrituraj1997@gmail.com>
2017-03-12 16:24:34 +0530

    Added Gradient function

--- Python_imp/costfunction.py
+++ Python_imp/costfunction.py
@@ -1,5 +1,6 @@
 import load_data
 import numpy as np
+from scipy import optimize
 
 X = load_data.X
 Y = load_data.Y
@@ -9,13 +10,19 @@ numusers = load_data.numusers
 nummovies = load_data.nummovies
 numfeatures = load_data.numfeatures
 
+X = X[0:nummovies, 0:numfeatures]
+Y = Y[0:nummovies,0:numusers]
+R = R[0:nummovies,0:numusers]
+Theta = Theta[0:numusers,0:numfeatures]
 
-def costfunction(X, Theta, Y, R, numusers, nummovies,numfeatures, reg = 0):
 
-    X = X[0:nummovies,0:numfeatures]
-    Y = Y[0:nummovies,0:numusers]
-    Theta = Theta[0:numusers, 0:numfeatures]
-    R = R[0:nummovies, 0:numusers]
+params = np.concatenate((Theta.reshape(np.prod(Theta.shape), 1), X.reshape(np.prod(X.shape), 1)))
+
+
+def costfunction(Params, Y, R, numusers, nummovies,numfeatures, reg = 0):
+
+    Theta = np.reshape(params[0:numusers * numfeatures, :], (numusers, numfeatures))
+    X = np.reshape(params[numusers*numfeatures: ,:], (nummovies, numfeatures))
     
 
     Output = np.dot(X, np.transpose(Theta))
@@ -23,6 +30,16 @@ def costfunction(X, Theta, Y, R, numusers, nummovies,numfeatures, reg = 0):
     J = 0.5 * np.sum((Output[R]- Y[R])*(Output[R] - Y[R])) + reg * np.sum(Theta *Theta) + reg * np.sum(X * X)
     return J
 
-costfunction(X,Theta, Y, R, numusers, nummovies, numfeatures)
+def gradientfunc(params, Y, R, numusers, nummovies,numfeatures, reg = 0):
+    Theta = np.reshape(params[0:numusers * numfeatures, :], (numusers, numfeatures))
+    X = np.reshape(params[numusers * numfeatures:, :], (nummovies, numfeatures))
 
+    Output = np.dot(X, np.transpose(Theta))
+
+    Grad_X = np.dot(R * Output - R * Y, Theta) + reg * X
+    Grad_Theta = np.dot(np.transpose(R * Output - R * Y), X) + reg * Theta
+
+    Gradient = np.concatenate((Grad_Theta.reshape(np.prod(Grad_Theta.shape), 1), Grad_X.reshape(np.prod(Grad_X.shape), 1)))
+
+    return Gradient
 
