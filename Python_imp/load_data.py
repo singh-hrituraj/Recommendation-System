c2e94088b76df15659780f621a4d1b3d70a81c21
Hrituraj Singh <hrituraj1997@gmail.com>
2017-03-16 22:16:16 +0530

    Update load_data.py

--- Python_imp/load_data.py
+++ Python_imp/load_data.py
@@ -1,21 +1,21 @@
 import scipy.io as sio
 
 
-data = sio.loadmat('ex8_movies.mat') #load the movie data set
+data = sio.loadmat('courses_data.mat') #load the courses data set
 
 Y = data['Y']
 R = data['R']
 
 
-Parameters = sio.loadmat('ex8_movieParams.mat')
+Parameters = sio.loadmat('course_parameters.mat')
 
 X = Parameters['X']
 
 Theta = Parameters['Theta']
 
-numstudents = Parameters['num_users']
+numstudents = Parameters['num_students']
 numfeatures = Parameters['num_features']
-numcourses = Parameters['num_movies']
+numcourses = Parameters['num_courses']
 
 
 
